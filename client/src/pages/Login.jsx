afdb01117d9ae1b962b7ee2877634818cd5da696
Punvireakroth <vireakrothpun@gmail.com>
2023-06-12 14:03:11 +0700

    Authentication UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { BsBoxArrowInRight } from "react-icons/bs";
 
 function Login() {
+  const onSubmit = () => {};
+  const onChange = () => {};
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <BsBoxArrowInRight /> Login
+        </h1>
+        <p>Login to your account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,66 @@
 import React from "react";
-
+import { BsFillPersonFill } from "react-icons/bs";
 function Register() {
+  const onChange = () => {};
+  const onSubmit = () => {};
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <BsFillPersonFill /> Register
+        </h1>
+        <p>Create an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              placeholder="Name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              placeholder="Confirm Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
 
