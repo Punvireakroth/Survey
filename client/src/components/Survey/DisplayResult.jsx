95dedcc8b65200b7df4791f649ef24258e76e9b2
Punvireakroth <vireakrothpun@gmail.com>
2023-06-27 14:18:23 +0700

    latest update of DisplayResult Component

--- client/src/components/Survey/DisplayResult.jsx
+++ client/src/components/Survey/DisplayResult.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { ShortResponseResult } from "./displayResultComponents";
 import { Container, Spinner } from "react-bootstrap";
