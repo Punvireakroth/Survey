c0b480874cb15aa70841e5d83bcd1fff4b29ee60
Punvireakroth <vireakrothpun@gmail.com>
2023-07-06 16:45:23 +0700

    update users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,33 @@
+// const express = require("express");
+// const {
+//   getUser,
+//   createUser,
+//   loginUser,
+//   updateUserSurveys,
+//   deleteUser,
+// } = require("../controllers/userController");
+// const router = express.Router();
+
+// router.get("/getUser", getUser);
+// router.post("/register", createUser);
+// router.post("/login", loginUser);
+// router.put("/updateSurveys/:id", updateUserSurveys);
+// router.delete("/delete/:id", deleteUser);
+
+// module.exports = router;
+
+// ----------------------------------------------------------------------------
+
 const express = require("express");
-const {
-  getUser,
-  createUser,
-  loginUser,
-  updateUserSurveys,
-  deleteUser,
-} = require("../controllers/userController");
+
+const { signupUser, loginUser } = require("../controllers/userController");
+
 const router = express.Router();
 
-router.get("/getUser", getUser);
-router.post("/register", createUser);
+// login route
 router.post("/login", loginUser);
-router.put("/updateSurveys/:id", updateUserSurveys);
-router.delete("/delete/:id", deleteUser);
+
+// signup route
+router.post("/signup", signupUser);
 
 module.exports = router;
