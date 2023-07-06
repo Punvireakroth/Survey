ab4c8bf1f29eb4b574f330fb6b61ee5cab66f170
Punvireakroth <vireakrothpun@gmail.com>
2023-07-06 16:44:33 +0700

    update user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,121 +1,163 @@
-const asyncHandler = require("express-async-handler");
+// const asyncHandler = require("express-async-handler");
+// const User = require("../models/userModel");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+
+// // @desc Get the user
+// // @route GET /users
+// // @access private ------------------
+
+// const getUser = asyncHandler(async (req, res) => {
+//   const user = await User.findOne({ _id: req.id });
+//   res.status(200).json(user);
+// });
+
+// // @desc login or register a user
+// // @route POST /user/login
+// // @access private ------------------
+
+// const createUser = asyncHandler(async (req, res) => {
+//   const { name, email, password } = req.body;
+//   const user = await User.findOne({ email: email }).exec();
+
+//   // Hash password
+//   const salt = await bcrypt.genSalt(10);
+//   const hashedPassword = await bcrypt.hash(password, salt);
+
+//   if (user) {
+//     res.status(200).json({ message: "User is already exist" });
+//   } else {
+//     const newUser = await User.create({
+//       name: name,
+//       email: email,
+//       password: hashedPassword,
+//     });
+//     if (newUser) {
+//       res.status(200).json({
+//         _id: newUser._id,
+//         name: name,
+//         email: email,
+//         token: generateToken(newUser._id),
+//       });
+//     } else {
+//       res.status(400);
+//       throw new Error("Invalid user data");
+//     }
+//   }
+// });
+
+// // @desc login user
+// // @route POST /user/login
+// // @access private
+
+// const loginUser = asyncHandler(async (req, res) => {
+//   const { email, password } = req.body;
+
+//   const user = await User.findOne({ email });
+
+//   if (user && (await bcrypt.compare(password, user.password))) {
+//     res.json({
+//       _id: user.id,
+//       name: user.name,
+//       email: user.email,
+//       token: generateToken(user._id),
+//     });
+//   } else {
+//     res.status(400);
+//     throw new Error("Invalid credentials");
+//   }
+// });
+
+// // @desc Update user survey
+// // @route GET /user/login
+// // @access private ------------------
+
+// const updateUserSurveys = asyncHandler(async (req, res) => {
+//   const user = await User.findById(req.params.id);
+//   if (!user) {
+//     res.status(400);
+//     throw new Error("No user was found");
+//   } else if (
+//     user.surveys.findIndex((survey) => survey === req.body.survey_id)
+//   ) {
+//     res.status(200).json({ message: "This survey already existed" });
+//   } else {
+//     console.log(req.body.survey_id);
+//     const newSurveys = [...user.surveys];
+//     newSurveys.push(req.body.survey_id);
+//     const updatedUser = await User.findOneAndUpdate(
+//       { _id: req.params.id },
+//       { surveys: newSurveys }
+//     );
+//     res.status(200).json(updatedUser);
+//   }
+// });
+
+// // @desc Delete user
+// // @route DELETE /users/delete/:id
+
+// const deleteUser = asyncHandler(async (req, res) => {
+//   const user = await User.findById(req.params.id);
+//   if (!user) {
+//     res.status(400);
+//     throw new Error("No user was found");
+//   }
+//   await User.deleteOne({ _id: req.params.id });
+//   const existState = await User.findOne({ _id: req.params.id });
+//   res.status(200).json({ id: req.params.id, existState });
+// });
+
+// // Generate json web token
+// const generateToken = (id) => {
+//   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+// };
+
+// module.exports = {
+//   getUser,
+//   createUser,
+//   loginUser,
+//   updateUserSurveys,
+//   deleteUser,
+// };
+
+// ----------------------------------------------------------------------------
+
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
-// @desc Get the user
-// @route GET /users
-// @access private ------------------
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
-const getUser = asyncHandler(async (req, res) => {
-  const user = await User.findOne({ _id: req.id });
-  res.status(200).json(user);
-});
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
 
-// @desc login or register a user
-// @route POST /user/login
-// @access private ------------------
+    // create token
+    const token = createToken(user._id);
 
-const createUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = await User.findOne({ email: email }).exec();
-
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  if (user) {
-    res.status(200).json({ message: "User is already exist" });
-  } else {
-    const newUser = await User.create({
-      name: name,
-      email: email,
-      password: hashedPassword,
-    });
-    if (newUser) {
-      res.status(200).json({
-        _id: newUser._id,
-        name: name,
-        email: email,
-        token: generateToken(newUser._id),
-      });
-    } else {
-      res.status(400);
-      throw new Error("Invalid user data");
-    }
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-});
+};
 
-// @desc login user
-// @route POST /user/login
-// @access private
-
-const loginUser = asyncHandler(async (req, res) => {
+// signup a user
+const signupUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+  try {
+    const user = await User.signup(email, password);
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Invalid credentials");
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-});
-
-// @desc Update user survey
-// @route GET /user/login
-// @access private ------------------
-
-const updateUserSurveys = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
-  if (!user) {
-    res.status(400);
-    throw new Error("No user was found");
-  } else if (
-    user.surveys.findIndex((survey) => survey === req.body.survey_id)
-  ) {
-    res.status(200).json({ message: "This survey already existed" });
-  } else {
-    console.log(req.body.survey_id);
-    const newSurveys = [...user.surveys];
-    newSurveys.push(req.body.survey_id);
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: req.params.id },
-      { surveys: newSurveys }
-    );
-    res.status(200).json(updatedUser);
-  }
-});
-
-// @desc Delete user
-// @route DELETE /users/delete/:id
-
-const deleteUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
-  if (!user) {
-    res.status(400);
-    throw new Error("No user was found");
-  }
-  await User.deleteOne({ _id: req.params.id });
-  const existState = await User.findOne({ _id: req.params.id });
-  res.status(200).json({ id: req.params.id, existState });
-});
-
-// Generate json web token
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-module.exports = {
-  getUser,
-  createUser,
-  loginUser,
-  updateUserSurveys,
-  deleteUser,
-};
+module.exports = { signupUser, loginUser };
