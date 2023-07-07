8e98b2ad6146e2e0812f295df4cc3cd413f9ecef
Punvireakroth <vireakrothpun@gmail.com>
2023-07-07 16:20:48 +0700

    get user ifo from localStorage

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -19,6 +19,12 @@ export const AuthContextProvider = ({ children }) => {
   });
 
   // set initial auth state
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
 
   console.log("AuthContext state: ", state);
 
