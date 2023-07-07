fd1924bf4851ce98a729702cc5ec75f8589c9b7e
Punvireakroth <vireakrothpun@gmail.com>
2023-07-07 16:19:48 +0700

    wrap AuthContextProvider Globally

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { AuthContextProvider } from "./context/AuthContext";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -12,13 +10,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <AuthContextProvider>
       <App />
-    </Provider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
