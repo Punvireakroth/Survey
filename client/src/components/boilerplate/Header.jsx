f0d3682add6ac0dacaa47194068052317ecb22ce
Punvireakroth <vireakrothpun@gmail.com>
2023-07-08 13:25:34 +0700

    remove React-Redux from the header

--- client/src/components/boilerplate/Header.jsx
+++ client/src/components/boilerplate/Header.jsx
@@ -3,23 +3,12 @@ import {
   BsBoxArrowInLeft,
   BsFillPersonFill,
 } from "react-icons/bs";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { logout, reset } from "../../features/auth/authSlice";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
-
   const onLogOut = () => {
-    if (user) {
-      dispatch(logout());
-      dispatch(reset());
-      navigate("/");
-    }
+    console.log("You are logout");
   };
-
   return (
     <div>
       <header className="header">
@@ -37,32 +26,29 @@ export default function Header() {
           </Link>
         </div>
         <ul>
-          {user ? (
-            <>
-              <li>
-                <button
-                  className="btn"
-                  onClick={onLogOut}
-                  style={{ color: "#fff", fontSize: "1.2rem", fontWeight: 400 }}
-                >
-                  <BsBoxArrowInLeft /> Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/login">
-                  <BsBoxArrowInRight /> Login
-                </Link>
-              </li>
-              <li>
-                <Link to="/register">
-                  <BsFillPersonFill /> Register
-                </Link>
-              </li>
-            </>
-          )}
+          <>
+            <li>
+              <button
+                className="btn"
+                onClick={onLogOut}
+                style={{ color: "#fff", fontSize: "1.2rem", fontWeight: 400 }}
+              >
+                <BsBoxArrowInLeft /> Logout
+              </button>
+            </li>
+          </>
+          <>
+            <li>
+              <Link to="/login">
+                <BsBoxArrowInRight /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <BsFillPersonFill /> Register
+              </Link>
+            </li>
+          </>
         </ul>
       </header>
     </div>
