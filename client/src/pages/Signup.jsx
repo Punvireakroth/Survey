74d76e078c3f8219893c2275c9ec61454f2ab48a
Punvireakroth <vireakrothpun@gmail.com>
2023-07-11 17:02:03 +0700

    Apply Authentication to the new styled of Signup Form

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -13,25 +14,93 @@ const Signup = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Sign up</h3>
-      <label>Email: </label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
+    <div id="login-background">
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <Card
+              className="shadow"
+              style={{
+                padding: 20,
+                borderRadius: 15,
+                fontSize: 1.2 + "rem",
+                color: "#008cba",
+              }}
+            >
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2
+                    className="fw-bold mb-2 text-uppercase"
+                    style={{ color: "#0c66a9" }}
+                  >
+                    ISI Survey
+                  </h2>
+                  <p className="mb-5">User Signup</p>
+                  <div className="mb-3">
+                    <Form onSubmit={handleSubmit}>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">Email</Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Enter email"
+                          style={{ padding: 0.7 + "rem", color: "#008cba" }}
+                          onChange={(e) => setEmail(e.target.value)}
+                          value={email}
+                        />
+                      </Form.Group>
 
-      <label>Password: </label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
+                      <Form.Group
+                        className="mb-4"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label className="text-center">
+                          Password
+                        </Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Enter password"
+                          style={{ padding: 0.7 + "rem", color: "#008cba" }}
+                          onChange={(e) => setPassword(e.target.value)}
+                          value={password}
+                        />
+                      </Form.Group>
 
-      <button disabled={isLoading}>Sign up</button>
-      {error && <div>{error}</div>}
-    </form>
+                      <div className="d-grid">
+                        <Button
+                          disabled={isLoading}
+                          type="submit"
+                          style={{
+                            padding: 0.7 + "rem",
+                            fontSize: 1.3 + "rem",
+                            backgroundColor: "#0c66a9",
+                            borderColor: "#0c66a9",
+                          }}
+                        >
+                          SIGNUP
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0 text-center">
+                        Don't have an account?{" "}
+                        <a
+                          href="/signup"
+                          className="text-primary fw-bold"
+                          style={{ textDecoration: "none" }}
+                        >
+                          Login
+                        </a>
+                      </p>
+                    </div>
+                    {error && <div className="error">{error}</div>}
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
