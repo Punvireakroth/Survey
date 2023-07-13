995236a31e5c2a722e0df5060ded4125d46f0582
Punvireakroth <vireakrothpun@gmail.com>
2023-07-13 13:21:34 +0700

    Check user and redirect

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Navigate } from "react-router-dom";
 function Welcome() {
-  return (
+  const { user } = useAuthContext();
+
+  return !user ? (
     <>
       <div>Click on the links below to login or register</div>
       <Link to={"/login"}>Login</Link>
       <br />
       <Link to={"/register"}>Register</Link>
     </>
+  ) : (
+    <Navigate to="/dashboard" />
   );
 }
 
