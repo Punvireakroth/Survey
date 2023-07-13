b943fcf84812e06653f97bf1bb030c1afc9e57ce
Punvireakroth <vireakrothpun@gmail.com>
2023-07-13 16:51:34 +0700

    Always rediect to login page

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { Navigate } from "react-router-dom";
+
 function Welcome() {
   const { user } = useAuthContext();
 
   return !user ? (
     <>
-      <div>Click on the links below to login or register</div>
-      <Link to={"/login"}>Login</Link>
-      <br />
-      <Link to={"/register"}>Register</Link>
+      <Navigate to="/dashboard" />
     </>
-  ) : (
-    <Navigate to="/dashboard" />
-  );
+  ) : null;
 }
 
 export default Welcome;
