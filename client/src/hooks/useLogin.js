215b5de9a83bf94ab8a71c6743f5aa9ae8335700
Punvireakroth <vireakrothpun@gmail.com>
2023-07-14 16:02:11 +0700

    save role to localStorage with login

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -6,14 +6,14 @@ export const useLogin = () => {
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
-  const login = async (email, password) => {
+  const login = async (email, password, role) => {
     setIsLoading(true);
     setError(null);
 
     const response = await fetch("/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email, password, role }),
     });
     const json = await response.json();
 
