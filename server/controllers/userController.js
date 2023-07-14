3e444b0d1703f7cecc06f06d406830bd5f703bdd
Punvireakroth <vireakrothpun@gmail.com>
2023-07-14 16:03:32 +0700

    send role to user when receive request from front-end

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,7 +14,7 @@ const loginUser = async (req, res) => {
     // create token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token, role });
+    res.status(200).json({ email, token, role: user.role });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -30,7 +30,7 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ email, token, role });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
