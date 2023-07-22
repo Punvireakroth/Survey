88e1be4deb7310ca73dd973776e689ed0e767433
Punvireakroth <vireakrothpun@gmail.com>
2023-07-22 13:11:34 +0700

    prolonged jwt expiration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,9 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+  return jwt.sign({ _id }, process.env.JWT_SECRET, {
+    expiresIn: "10d",
+  });
 };
 
 // login user
